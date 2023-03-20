530f6285124264ede78dd009732fa28249ded0ab
Sarita Toivio <58324623+stoivio@users.noreply.github.com>
2023-03-20 16:43:35 +0200

    korjattu L08T03 ehtolauseke

--- 08-ehtolauseet/L08T03.py
+++ 08-ehtolauseet/L08T03.py
@@ -4,7 +4,7 @@ luku = int(input("Anna kokonaisluku: "))
 def check_int(param1):
     if param1 == 10 or param1 == 20:
         return "Luku on 10 tai 20"
-    elif param1 == 10 or param1 == 200:
+    elif param1 == 100 or param1 == 200:
         return "Luku on 100 tai 200"
     else:
         # muutoin palauta annettu luku tekstinä
